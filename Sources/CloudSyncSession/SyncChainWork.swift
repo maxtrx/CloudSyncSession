c3e7b7b3cf25c5b6c4c681b78cb1de1060d47586
maxtrx <xansoftwaremax@gmail.com>
2023-05-25 13:12:06 +0200

    Rename associated type

--- Sources/CloudSyncSession/SyncChainWork.swift
+++ Sources/CloudSyncSession/SyncChainWork.swift
@@ -9,13 +9,13 @@ import Foundation
 
 /// A work item that is part of a larger task that involves multiple requests made to CloudKit.
 public protocol SyncChainWork {
-    associatedtype SyncChainWorkResponse
+    associatedtype OperationResponse
     
     /// The next request to be dispatched after this request completed. If the parent work has been completed, it is `nil`.
     var successor: (any SyncChainWork)? { get }
     
     /// The response that is returned from the session for this particular work item.
-    var response: SyncChainWorkResponse? { get set }
+    var response: OperationResponse? { get set }
     
     /// Creates a `SyncOperation` and dispatches it to the session.
     func dispatch(to session: CloudSyncSession) throws
